37ca260739211c64519e217a20b462d4ebca5533
lee <aksndk123@naver.com>
2022-07-28 10:09:18 +0900

    ** 내가 약한 조건&순서도 문제
    잘 짜긴했는데 뭔가 정형화된 풀이는 여전히 못하겠음

---
+++ 힙/디스크컨트롤러.py
@@ -0,0 +1,30 @@
+import heapq
+def solution(jobs):
+    cnt = len(jobs)
+    jobs.sort(reverse=True)
+    answer = 0
+    hard = []
+    scheduler = []
+    now_time = 0
+    now_job = 0
+    while jobs:
+        # 처음 작업 넣기
+        if jobs:
+            now_job = jobs.pop()
+            now_time = now_job[0] + now_job[1]
+            answer += now_job[1]
+        while now_job != 0:
+            while jobs and now_time >= jobs[-1][0]:
+                a, b = jobs.pop()
+                heapq.heappush(scheduler, [b, a])
+            now_job = 0
+            if scheduler:
+                b, a = heapq.heappop(scheduler)
+                now_time += b
+                now_job = [a, b]
+                answer += (now_time - a)
+    return answer // cnt
+
+
+jobs = [[0, 3], [1, 9], [2, 6]]
+print(solution(jobs))

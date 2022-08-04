fc4839a43da1203e1dd73f54d548828076b21b92
lee <aksndk123@naver.com>
2022-08-04 16:18:16 +0900

    *** 익숙하지 않는 dfs연습 필요
    일단 a->b형식의 티켓인데 이를 다써서 끝나야한다는걸 알아함
     -> 즉, 체크를 해야하는게 경로에대해서 방문체크를 해야함
      --> a->b 이면 check[b] += 1해서 체크할려고 했는데 이렇게 하는경우 a->b, c->b 인데 a->b를 두번쓸수도 있으니 문제가 생김
      따라서, check[(a,b)]로 풀어버리니 쉽게 통과함

---
+++ dfs bfs/여행 경로.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+def dfs(now, check, graph):
+    global answer, n
+    if len(answer) == n:
+        return 1
+    for next in graph[now]:
+        if check[(now, next)] != 0:
+            check[(now, next)] -= 1
+            answer.append(next)
+            if dfs(next, check, graph):
+                return 1
+            check[(now, next)] += 1
+            answer.pop()
+    return 0
+
+
+def solution(tickets):
+    start = "ICN"
+    graph = defaultdict(list)
+    check = defaultdict(int)
+    for a, b in tickets:
+        graph[a].append(b)
+        check[(a, b)] += 1
+    for key in graph.keys():
+        graph[key].sort()
+    global answer, n
+    n = len(tickets) + 1
+    answer = [start]
+    dfs(start, check, graph)
+
+    return answer
+
+# tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+print(solution(tickets))
